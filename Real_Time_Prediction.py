02aff00e5ca7fff045343762bbdb6eb7d78af786
Joerg-ffs <40400868+Joerg-ffs@users.noreply.github.com>
2019-03-25 23:49:37 -0400

    Real time model prediction

--- Real_Time_Prediction.py
+++ Real_Time_Prediction.py
@@ -1,4 +1,6 @@
 import os
+import csv
+import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,108 +8,74 @@ import mss
 import mss.tools
 import evdev
 import cv2
-from evdev import InputDevice, categorize, ecodes
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Conv2D, Lambda, Dropout
-from keras.optimizers import Adam
+from evdev import InputDevice
+from keras.models import load_model
+from keras import backend as K
 
-IMG_WIDTH = 64
-IMG_HEIGHT = 64
-
+def coeff_determination(y_true, y_pred):
+    SS_res =  K.sum(K.square( y_true-y_pred ))
+    SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) )
+    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
 
 def main():
     # The screen part to capture
     # Camera Mode:6, windowed 720
-    monitor = {'top': 537, 'left': 866, 'width': 191, 'height': 74}
+    monitor = {'top': 437, 'left': 766, 'width': 400, 'height': 300}
     toggle_recording = False
     # Device path for steering wheel
     dev = InputDevice('/dev/input/event22')
     devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-    count = 0
-    statusList = [0, 0, 0, 0]
-
-    # statusList = [0,0]
-
-    def send_data(name, value):
-
-        # Fetching and recording the useful inputs
-        if name == "angle":
-            statusList[0] = value
-        elif name == "accelerator":
-            statusList[1] = value
-        elif name == "brake":
-            statusList[2] = value
-        elif name == "image":
-            statusList[1] = str(output)
-
-    # compile saved model
-    model = Sequential()
-    # Add lambda function to avoid pre-processing outside of network
-    col = 64
-    row = 64
-    ch = 3
-    model.add(Lambda(lambda x: x / 127.5 - 1.,
-                     input_shape=(col, row, ch),
-                     output_shape=(col, row, ch)))
-    model.add(Conv2D(3, 5, 5, input_shape=(64, 64, 3), subsample=(2, 2), activation='relu'))
-    model.add(Conv2D(24, 5, 5, input_shape=(32, 32, 3), subsample=(2, 2), activation='relu'))
-    model.add(Conv2D(36, 5, 5, input_shape=(5, 22, 3), subsample=(2, 2), activation='relu'))
-    model.add(Conv2D(48, 3, 3, input_shape=(3, 20, 3), activation='relu'))
-    model.add(Conv2D(64, 3, 3, input_shape=(1, 18, 3), activation='relu'))
-    model.add(Flatten())
-    model.add(Dense(1164, activation='linear'))
-    model.add(Dropout(.5))
-    model.add(Dense(100, activation='linear'))
-    model.add(Dropout(.2))
-    model.add(Dense(50, activation='linear'))
-    model.add(Dropout(.1))
-    model.add(Dense(10, activation='linear'))
-    model.add(Dense(1, activation='linear'))
-
-    model.load_weights("model_bal.h5")
+    prev_angles = []
+    model = load_model('Capstone2.h5', custom_objects={'coeff_determination': coeff_determination})
     # Initializing mss outside of while loop improves performance
     with mss.mss() as sct:
         while 1:
-
             for event in dev.read_loop():
 
                 if event.code == 295 and event.value != 0:
                     toggle_recording = not toggle_recording
 
                 elif toggle_recording == True:
+                        if event.code == 0 and event.value != 0:
+                            # steering angle
+                            #send_data('angle', event.value)
 
-                    if event.code == 2:
-                        # brake
-                        send_data('brake', event.value)
-                    elif event.code == 1:
-                        # accelerator
-                        send_data('accelerator', event.value)
-                    if event.value != 0 and event.code == 0:
-                        # steering angle
-                        send_data('angle', event.value)
+                            # Screen Capture, and image file storage
+                            output = '/media/joerg/A6CA2428CA23F36B/predict/test.png'
+                            # compressing the image
+                            sct.compression_level = 2
+                            # grab the data
+                            sct_img = sct.grab(monitor)
+                            # save to the picture file
+                            mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
 
-                        # Screen Capture, and image file storage
-                        output = '/media/joerg/OS_Install/Users/Joerg/Desktop/Capstone-Project-master/predict/real_img-1.png'
-                        # Compressing the image
-                        sct.compression_level = 2
-                        # Grab the data
-                        sct_img = sct.grab(monitor)
-                        # Save to the picture file
-                        mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
-                        send_data("image", output)
+                            img_array = cv2.imread(output, 3)
+                            img_array = img_array[80:212, 0:400]
+                            img_array = cv2.resize(img_array, (66, 200))
+                            X = np.array(img_array).reshape(-1, 66, 200, 3)
+                            pred_angle = model.predict(X, batch_size=1, verbose=0)[0][0]
+                            angle = (event.value-32752)/32752 * 2
+                            if len(prev_angles) < 20:
+                                prev_angles.append(pred_angle)
+                                prev_angles.append(angle)
 
-                        img_array = cv2.imread(output, 3)
-                        img_array = cv2.resize(img_array, (IMG_WIDTH, IMG_HEIGHT))
-                        X = img_array
-                        X = np.array(X).reshape(-1, IMG_WIDTH, IMG_HEIGHT,3)  # Trailing 1 represents 1 dimensions due to grayscale -> 3 for colour
-                        X = X / 255.0  # Normalize image data devide by 255 pixels
-
-                        y = model.predict(X, verbose=0)
-                        os.remove(output)
-
-                        # compare actual vs predicted angle
-                        print('Actual angle: ' + str(event.value))
-                        print('Predicted angle: ' + str(model.predict(np.array(X), verbose=0)))
+                            else:
+                                prev_angles.pop(0)
+                                prev_angles.pop(1)
+                                prev_angles.append(pred_angle)
+                                prev_angles.append(angle)
+                            smooth_angle = sum(prev_angles)/len(prev_angles)
+                            # compare actual vs predicted angle
+                            #print('Actual angle: ' + str(angle))
+                            #print('Predicted angle: ' + str(smooth_angle))
+                            print('Difference: ' + str(abs(smooth_angle - angle)))
+                            saveData = [angle, smooth_angle]
+                            
+                            # Writing the data to a csv file for training purposes
+                            time.sleep(0.025)
+                            with open(r"prediction5.csv", "a", newline='') as f:
+                                writer = csv.writer(f)
+                                writer.writerow(saveData)
 
                 elif toggle_recording == False:
                     print("Recording Paused")
